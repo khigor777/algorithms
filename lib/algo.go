86d9d6797d5083934875c3d1d9b271174f240d5a
Igor Khristenko <khigor777@gmail.com>
2017-10-10 22:55:15 +0300

    Another try SameBinarySearch

--- lib/algo.go
+++ lib/algo.go
@@ -17,3 +17,25 @@ func BinarySearch(item int, lists []int) int {
 	}
 	return 0
 }
+
+
+func SameBinarySearch(si int, in []int) (index int) {
+
+	low :=0
+	high := len(in) - 1
+
+	for low <= high {
+		mid := (low + high) / 2
+		cur := in[mid]
+		if cur == si{
+			return mid
+		}
+		if cur > si {
+			high = mid - 1
+		}else {
+			low = mid + 1
+		}
+
+	}
+	return 0
+}
--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	_"khigor777/algorithms/lib"
 	"khigor777/algorithms/lib"
 )
 
 func main() {
-	fmt.Println(lib.BinarySearch(7, []int{1, 2, 4, 5, 7, 8, 10, 12}))
+	fmt.Println(lib.SameBinarySearch(9, []int{1,2,3,4,5,6,7,8,9,10}))
 }
+
